8f794485637f07d30c4582fc999bac4a2eb93862
Gautamvala36 <gautamvala711@gmail.com>
2023-03-21 14:05:47 +0530

    first commit

---
+++ js/my.js
@@ -0,0 +1,8 @@
+ //without return value with argument
+ let html = function (id, message, isAdd = false) {
+    if (isAdd = false) {
+        document.getElementById(id).innerHTML = message;
+    }
+    else
+    document.getElementById(id).innerHTML += message;
+}
