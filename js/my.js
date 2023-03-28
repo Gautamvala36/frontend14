5caf00a9eec8825fda880d168adc3b06001af0b4
Gautamvala36 <gautamvala711@gmail.com>
2023-03-28 10:08:36 +0530

    .

--- js/my.js
+++ js/my.js
@@ -1,9 +1,13 @@
- //without return value with argument
- let html = function (id, message, isAdd = false) {
+//without return value with argument
+let html = function (id, message, isAdd = false) {
     if (isAdd = false) {
         document.getElementById(id).innerHTML = message;
     }
     else
-    document.getElementById(id).innerHTML += message;
+        document.getElementById(id).innerHTML += message;
 }
-let val = (id) =>document.getElementById(id).value; 
+let val = (id) => document.getElementById(id).value;
+//https://stackoverflow.com/questions/175739/how-can-i-check-if-a-string-is-a-valid-number
+function isNumeric(value) {
+    return /^-?\d+$/.test(value);
+}
